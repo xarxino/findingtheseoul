ef67f275148d8cff2aa30bf8fe31dd0e9f214ec3
graafmans.tom@gmail.com <xarxino>
2023-04-22 15:26:45 +0200

    Removed empty walker

--- functions.php
+++ functions.php
@@ -22,5 +22,4 @@ require_once('includes/enqueue.php'); // Loads theme styles and scripts.
 require_once('includes/livesearch.php'); // Loads live search functions.
 require_once('includes/register.php'); // Loads register scripts.
 require_once('includes/templates.php'); // Loads theme templates.
-require_once('includes/walker.php'); // Loads custom walker functions.
 require_once('includes/breadcrumbs.php'); // Loads breadcrumbs functions.
