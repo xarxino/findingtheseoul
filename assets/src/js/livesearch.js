715b1bf8ccb8701a8a6fb37f1cb15983649e6db7
graafmans.tom@gmail.com <xarxino>
2023-04-22 15:29:13 +0200

    Created live search form

--- assets/src/js/livesearch.js
+++ assets/src/js/livesearch.js
@@ -1,42 +1,73 @@
 const searchInput = document.querySelector(".live-search-input");
-const searchResults = document.querySelector(".live-search-results");
-let searchTimeout;
+let searchResults = document.querySelector(".live-search-results");
 
-function createColumn(title, items, query, isPost = false) {
+if (searchResults) {
+  searchInput.addEventListener("input", () => {
+    const query = encodeURIComponent(searchInput.value.trim());
+
+    if (query) {
+      searchResults.classList.remove("hidden");
+      searchResults.classList.add("flex");
+      updateSearchResults(query);
+    } else {
+      searchResults.innerHTML = "";
+      searchResults.classList.add("hidden");
+      searchResults.classList.remove("flex");
+    }
+  });
+}
+
+function createColumn(title, items, query, isPost = false, archiveLink = "") {
   const regex = new RegExp(`(${query})`, "ig");
   const filteredItems = items.filter((item) =>
     (isPost ? item.title : item.name).match(regex)
   );
-  const listItems = filteredItems.length
-    ? filteredItems
+  const slicedItems = filteredItems.slice(0, 5);
+  const listItems = slicedItems.length
+    ? slicedItems
         .map((item) => {
           const itemName = isPost ? item.title : item.name;
           const name = itemName.replace(regex, "<strong>$1</strong>");
-          return `<li><a class="hover:underline" href="${
+          return `<li class="border-b last-of-type:border-none border-gray-200 py-2"><a class="hover:underline" href="${
             item.url || item.permalink
           }">${name}</a></li>`;
         })
         .join("")
-    : "<li>No results found</li>";
+    : "";
+
+  const noResultsMessage = "<li>No results found</li>";
+  const moreButton =
+    filteredItems.length > 5
+      ? `<a href="${archiveLink}" class="mt-2 inline-block px-4 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded">More ${title}</a>`
+      : "";
 
   return `
     <div class="flex flex-col gap-4">
       <h2 class="text-lg font-bold">${title}</h2>
-      <ul class="flex flex-col gap-2">${listItems}</ul>
+      <ul class="flex flex-col gap-2">
+        ${listItems ? listItems : noResultsMessage}
+      </ul>
+      ${moreButton}
     </div>`;
 }
 
 function updateSearchResults(query) {
-  if (!query || query === "") {
+  if (!query) {
     searchResults.innerHTML = "";
     searchResults.classList.add("hidden");
     searchResults.classList.remove("flex");
     return;
   }
 
-  fetch(
-    `${livesearch_ajax_object.ajax_url}?action=my_live_search&query=${query}`
-  )
+  // Display rotating icon for loading animation
+  searchResults.innerHTML = `
+    <div class="loading-animation flex justify-center items-center">
+      <svg class="animate-spin h-5 w-5 text-gray-500" viewBox="0 0 24 24">
+        <path d="M12 2a9 9 0 00-9 9 1 1 0 002 0 7 7 0 0114 0 1 1 0 102 0 9 9 0 00-9-9z" fill="currentColor" />
+      </svg>
+    </div>
+  `;
+  fetch(`${livesearch_ajax_object.ajax_url}?action=livesearch&query=${query}`)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -46,29 +77,105 @@ function updateSearchResults(query) {
     })
     .then((data) => {
       if (data && data.success) {
-        const { posts, categories, tags } = data.data;
+        const { posts, categories, tags, pages } = data.data;
+
+        const postsColumn = createColumn(
+          "Posts",
+          posts,
+          query,
+          true,
+          livesearch_ajax_object.post_archive_link
+        );
+        const categoriesColumn = createColumn(
+          "Categories",
+          categories,
+          query,
+          false,
+          livesearch_ajax_object.categories_archive_link
+        );
+        const tagsColumn = createColumn(
+          "Tags",
+          tags,
+          query,
+          false,
+          livesearch_ajax_object.tags_archive_link
+        );
+        const pagesColumn = createColumn(
+          "Pages",
+          pages,
+          query,
+          true,
+          livesearch_ajax_object.pages_archive_link
+        );
+
         searchResults.innerHTML = `
-        ${createColumn("Posts", posts, query, true)}
-        ${createColumn("Categories", categories, query)}
-        ${createColumn("Tags", tags, query)}`;
+          <div class="flex flex-col gap-4">
+            ${postsColumn}
+            ${categoriesColumn}
+            ${tagsColumn}
+            ${pagesColumn}
+          </div>`;
       } else {
-        searchResults.innerHTML = "<li>Error: Unknown error</li>";
+        const postsColumn = createColumn("Posts", [], query, true);
+        const categoriesColumn = createColumn("Categories", [], query);
+        const tagsColumn = createColumn("Tags", [], query);
+        const pagesColumn = createColumn("Pages", [], query, true);
+
+        searchResults.innerHTML = `
+          <div class="flex flex-col gap-4">
+            ${postsColumn}
+            ${categoriesColumn}
+            ${tagsColumn}
+            ${pagesColumn}
+          </div>`;
       }
     })
     .catch((error) => {
-      searchResults.innerHTML = `<li>Error: ${error.message}</li>`;
+      searchResults.innerHTML =
+        "<p class='text-center'>Error retrieving search results</p>";
+      console.error("Error fetching search results:", error);
     });
 }
 
-searchInput.addEventListener("input", () => {
-  clearTimeout(searchTimeout);
-  const query = encodeURIComponent(searchInput.value.trim());
+// Hide search results when clicking outside the search results container
+document.addEventListener("click", (event) => {
+  if (!searchResults.contains(event.target) && event.target !== searchInput) {
+    searchResults.classList.add("hidden");
+    searchResults.classList.remove("flex");
+  }
+});
 
-  if (query) {
+// Hide search results and clear the container when clicking outside the search input and search results container
+document.addEventListener("scroll", () => {
+  if (!searchInput.matches(":focus")) {
+    searchResults.classList.add("hidden");
+    searchResults.classList.remove("flex");
+  }
+});
+
+// Hide search results when scrolling, if the search input is not focused
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    searchResults.classList.add("hidden");
+    searchResults.classList.remove("flex");
+  }
+});
+
+// Hide search results when the ESC key is pressed
+document.addEventListener("click", (event) => {
+  if (
+    !event.target.closest(".live-search-input") &&
+    !event.target.closest(".live-search-results")
+  ) {
+    searchResults.classList.add("hidden");
+    searchResults.classList.remove("flex");
+  }
+});
+
+// Show search results when clicking the search input, if it already contains a value
+searchInput.addEventListener("click", () => {
+  if (searchInput.value.trim() !== "") {
     searchResults.classList.remove("hidden");
     searchResults.classList.add("flex");
-    searchTimeout = setTimeout(() => updateSearchResults(query), 250);
-  } else {
-    updateSearchResults(query);
   }
 });
---
+++ templates/components/search-form.php
@@ -0,0 +1,24 @@
+<div class="relative">
+    <form role="search" method="get" class="flex items-center" action="<?= esc_url(home_url('/')); ?>">
+        <div class="relative flex items-center gap-8">
+            <label for="search-input" class="sr-only">
+                <?= esc_attr__('Search', 'textdomain'); ?>
+            </label>
+            <input type="search" id="search-input" placeholder="<?= esc_attr__('Search', 'textdomain'); ?>" name="s"
+                value="<?= get_search_query(); ?>"
+                class="flex-1 py-2 pl-4 pr-12 h-10 text-black rounded-full border border-gray-200 focus:outline-none focus:ring-2 focus:ring-red-700 focus:border-transparent transition min-w-full lg:min-w-[25rem] live-search-input"
+                autocomplete="off" />
+            <button type="submit"
+                class="absolute inset-y-0 right-0 flex items-center justify-center w-10 h-10 px-4 text-white transition bg-red-700 rounded-full hover:bg-red-800"
+                aria-label="<?= esc_attr__('Submit search', 'textdomain'); ?>">
+                <span class="dashicons dashicons-search"></span>
+            </button>
+        </div>
+    </form>
+    <div class="absolute left-0 z-20 flex-col hidden w-full gap-4 p-8 mt-2 bg-white border border-gray-200 shadow-md rounded-b-md live-search-results"
+        aria-live="polite">
+        <div class="hidden loading-animation">
+            <?= esc_html__('Loading...', 'textdomain'); ?>
+        </div>
+    </div>
+</div>
