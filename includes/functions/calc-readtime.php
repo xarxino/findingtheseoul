663a09f9a4423bf68180a4e4e06204a4059bc78d
graafmans.tom@gmail.com <xarxino>
2023-04-24 18:46:06 +0200

    Removed all regular PHP files

--- includes/functions/calc-readtime.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Define the calculateReadingTime() function in custom-functions.php
-function calculateReadingTime($content)
-{
-    $word_count = str_word_count(strip_tags($content));
-    $reading_time = ceil($word_count / 200); // assuming 200 words per minute reading speed
-    echo '<span class="dashicons dashicons-clock"></span>' . $reading_time . ' ' . esc_html__('min.', 'textdomain');
-}
--- templates/components/newsletter-form.php
+++
@@ -1,7 +0,0 @@
-<div class="relative z-10 flex flex-col items-center justify-center gap-16 py-24 bg-zinc-100 after:bg-hangul after:bg-center after:bg-50% after:bg-repeat after:-z-10 after:opacity-[3%] after:absolute after:-inset-0">
-    <h3>Monthly Korea insights, straight to your inbox</h3>
-    <form class="flex flex-col gap-4 md:flex-row md:gap-8">
-        <input type="email" name="email" placeholder="Your email address" class="px-4 py-2 border rounded-full border-black/20 min-w-[400px] bg-zin-100 focus:outline-red-700">
-        <button type="submit" class="px-6 py-3 text-white transition bg-red-700 rounded-full hover:bg-red-800">Subscribe</button>
-    </form>
-</div>
